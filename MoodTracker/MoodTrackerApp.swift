950abef985b9613979119be5e8c061c77478e9d2
Olivia Xu <olivixu@gmail.com>
2025-10-30 20:46:58 -0400

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Olivia Xu on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodTrackerTests/MoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerTests.swift
+//  MoodTrackerTests
+//
+//  Created by Olivia Xu on 10/30/25.
+//
+
+import Testing
+@testable import MoodTracker
+
+struct MoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
